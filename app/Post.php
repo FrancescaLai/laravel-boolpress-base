2e07390b12c393a861d50a63a33f266e556f83e2
FrancescaLai <francescalai1690@gmail.com>
2021-05-24 16:20:02 +0200

    "PostController" created + Route::resource (in web.php) implemented

--- app/Post.php
+++ app/Post.php
@@ -2,11 +2,14 @@
 
 namespace App;
 
-use App\Comment;
-
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
     protected $guarded = [];
+
+    public function comments()
+    {
+        return $this->hasMany('App\Comment');
+    }
 }
