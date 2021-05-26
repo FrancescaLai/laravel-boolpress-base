837d445a256ab0efd585b1ec05e2e0cf5066fd45
FrancescaLai <francescalai1690@gmail.com>
2021-05-26 17:06:03 +0200

    Create page implemented

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -4,14 +4,44 @@
     Crea un nuovo post
 @endsection
 
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
 @section('content')
 <form action="{{route('admin.posts.store')}}" method="POST">
+  @method('POST')
+  @csrf
+
     <div class="form-group">
-      <label for="exampleInputEmail1">Email address</label>
-      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+      <label for="title">Titolo</label>
+      <input type="text" class="form-control" id="title" placeholder="Title" name="title">
+    </div>
+    <div class="form-group">
+      <label for="title">Data</label>
+      <input type="date" class="form-control" id="date" placeholder="Date" name="date">
+    </div>
+    <div class="form-group">
+      <label for="content">Contenuto</label>
+      <textarea class="form-control" id="content" placeholder="content" name="content" cols="30" rows="10"></textarea>
+    </div>
+    <div class="form-group">
+      <label for="image">Immagine</label>
+      <input type="text" class="form-control" id="image" placeholder="image" name="image">
+    </div>
+    <div class="form-check form-check-inline">
+      <input class="form-check-input" type="checkbox" id="published" name="published">
+      <label class="form-check-label" for="published">Pubblicato</label>
+    </div>
+    <div class="mt-3">
+      <button type="submit" class="btn btn-primary">Crea</button>
     </div>
     
-    <button type="submit" class="btn btn-primary">Submit</button>
   </form>
 @endsection
